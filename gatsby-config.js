2c5751cee757374f0577179fd43faabe3828d493
Ardillán <ardillan@me.com>
2019-05-01 23:37:15 +0200

    Añade PrismJS

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,23 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
