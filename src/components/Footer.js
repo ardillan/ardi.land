6da2eb989f36c0e3a04ebbe0517dcab5b90d367d
Ardillán <ardillan@me.com>
2019-05-14 19:20:03 +0200

    Actualiza footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,60 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-const Footer = () => {
+export default ({ data }) => {
   return (
-    <footer>
-      <div>
-        <p>
-          {" "}
-          Esta web está hecha con <a href="http://gatsbyjs.org/">GatsbyJS</a> y
-          algo de café desde la verde y gris{" "}
-          <a href="https://es.wikipedia.org/wiki/Torrelavega">Torrelavega</a>
-        </p>
-      </div>
-    </footer>
+    <StaticQuery
+      query={graphql`
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              social {
+                twitter
+                instagram
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <footer>
+            <div>
+              <p>
+                {" "}
+                Esta web está hecha con{" "}
+                <a href="http://gatsbyjs.org/">GatsbyJS</a> y algo de café desde
+                la verde y gris{" "}
+                <a href="https://es.wikipedia.org/wiki/Torrelavega">
+                  Torrelavega
+                </a>
+              </p>
+            </div>
+            <div>
+              <span>
+                <a
+                  href={`https://twitter.com/${
+                    data.site.siteMetadata.social.twitter
+                  }`}
+                >
+                  Twitter
+                </a>
+              </span>
+              <span>
+                <a
+                  href={`https://instagram.com/${
+                    data.site.siteMetadata.social.instagram
+                  }`}
+                >
+                  Instagram
+                </a>
+              </span>
+            </div>
+          </footer>
+        )
+      }}
+    />
   )
 }
-
-export default Footer
