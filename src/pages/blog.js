0a185173278d2f263e24d08d8f1dd93441ffc779
Pablo Vila <pablo.vilafer@gmail.com>
2019-05-24 17:28:34 +0200

    added title to every page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
 import PostsList from "../components/PostsList"
+import SEO from "../components/SEO"
 import { graphql } from "gatsby"
 
 export default ({ data }) => {
   return (
     <Layout>
+      <SEO title="Ardillan.com | Blog" />
       <h1>Blog</h1>
       <h2>En esta página se muestran todas las entradas del blog.</h2>
       <PostsList length="50" type="blog" />
