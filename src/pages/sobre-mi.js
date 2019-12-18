4071869c89d67bbcd1f11490297699081dbdb68d
Ardillán <ardillan@me.com>
2019-12-18 19:54:22 +0100

    Actualiza información

--- src/pages/sobre-mi.js
+++ src/pages/sobre-mi.js
@@ -7,7 +7,10 @@ import Hello from "../components/Hello"
 import Img from "gatsby-image"
 
 export default ({ data }) => {
-  console.log(data)
+  const ToDoText = () => (
+    <img src="https://via.placeholder.com/700x200" alt="Imagen de prueba" />
+  )
+
   return (
     <Layout>
       <SEO title="Ardillan.com | Sobre mí" />
@@ -36,6 +39,42 @@ export default ({ data }) => {
         trabajos de cartelería, imagen corporativa o portadas para libros y
         discos.
       </p>
+
+      <p>
+        Estos son{" "}
+        <strong>algunos de mis trabajos como diseñador gráfico</strong>:
+      </p>
+
+      <h3>Coders Cantabria</h3>
+
+      <blockquote>
+        Coders Cantabria es una asociación sin ánimo de lucro que organiza
+        charlas donde se habla de todo lo relacionado con el mundo de desarrollo
+        de software.
+      </blockquote>
+
+      <p>
+        He tenido la oportunidad de ayudar a Coders Cantabria desarrollando su
+        imagen de marca.{" "}
+      </p>
+
+      <ToDoText />
+
+      <h3>Cartelería</h3>
+      <p>
+        A lo largo del tiempo, he diseñado multitud de carteles para diferentes
+        eventos. Estos son algunos de ellos:
+      </p>
+
+      <ToDoText />
+
+      <h3>Portadas</h3>
+      <p>
+        De la misma manera, he tenido la oportunidad de dar cara a libros o
+        discos. Aquí algunos ejemplos:{" "}
+      </p>
+
+      <ToDoText />
     </Layout>
   )
 }
