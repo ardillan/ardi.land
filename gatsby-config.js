128dd5cf5126e7e1999b51b8eaf3433e69fc764e
Ardillán <ardillan@me.com>
2019-12-18 19:07:58 +0100

    Añade dependencias

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
