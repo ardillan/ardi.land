983a1feb410abed6e2f016be00a33794e49f4984
Ardillán <ardillan@me.com>
2019-12-26 19:36:46 +0100

    Elimina código innecesario

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,13 +55,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `data`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/content/images/`,
       },
