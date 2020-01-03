e559cff622a2f9971df88a3b5f3fdd09bdbcc330
Ardillán <ardillan@me.com>
2020-01-03 19:55:03 +0100

    Añade nueva configuración

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,59 +60,10 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
-            },
-            query: `
-            {
-              allMarkdownRemark(
-                sort: {order: DESC, fields: [frontmatter___date]}, 
-                filter: {frontmatter: {type: {eq: "blog"}}}) {
-                edges {
-                  node {
-                    excerpt
-                    html
-                    fields {
-                      slug
-                    }
-                    frontmatter {
-                      title
-                      date
-                    }
-                  }
-                }
-              }
-            }            
-            `,
-            output: "/rss.xml",
-            title: "Listado de entradas del blog de Ardillán",
-            match: "^/blog/",
-          },
-        ],
+        name: `images`,
+        path: `${__dirname}/src/data/`,
       },
     },
   ],
