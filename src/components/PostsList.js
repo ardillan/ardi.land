cf6b065ad5a49b087805d7af534dab4eec9b2fb7
Ardillán <ardillan@me.com>
2020-09-27 16:32:00 +0200

    Añade buscador a blog

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
@@ -9,6 +9,34 @@ import { useGetGenericFeaturedImage } from "../hooks/useGetGenericFeaturedImage"
 
 import { formatDate } from "../utils/helpers"
 
+const SearchContainer = styled.div`
+  padding: 20px 0;
+  margin: -40px 0 40px 0;
+  display: flex;
+  input {
+    background: ${(props) => props.theme.colors.background.main};
+    color: ${(props) => props.theme.colors.fonts.text};
+    border-radius: 50px;
+    border: 2px solid #ea0054;
+    border: 2px solid ${(props) => props.theme.colors.fonts.anchor};
+    height: 45px;
+    margin: 0;
+    padding: 0 30px;
+    width: 100%;
+
+    ::placeholder {
+      color: ${(props) => props.theme.colors.fonts.text}54;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  @media screen and (max-width: ${(props) => props.theme.breakPoints.desktop}) {
+    padding: 20px;
+  }
+`
 const PostsListContainer = styled.ul`
   display: grid;
   grid-row-gap: 40px;
@@ -30,7 +58,6 @@ const PostsListContainer = styled.ul`
     margin: auto;
   }
 `
-
 const Post = styled.li`
   article {
     display: flex;
@@ -85,7 +112,6 @@ const Post = styled.li`
     }
   }
 `
-
 const Category = styled.div`
   display: flex;
   span {
@@ -101,7 +127,6 @@ const Category = styled.div`
     justify-content: center;
   }
 `
-
 const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -110,49 +135,79 @@ const Content = styled.div`
 
 export default (props) => {
   const posts = props.featured ? useGetFeaturedPosts() : useGetAllPosts()
-  return (
-    <PostsListContainer>
-      {posts.slice(0, props.length).map((post) => {
-        let featuredImage =
-          post.node.frontmatter.featuredImage !== null
-            ? post.node.frontmatter.featuredImage.childImageSharp.fixed
-            : useGetGenericFeaturedImage().childImageSharp.fluid
 
-        return (
-          <Post key={post.node.id}>
-            <Link to={`/${post.node.fields.slug}`} key={post.node.id}>
-              <article>
-                <header>
-                  <Img
-                    fixed={featuredImage}
-                    fadeIn={true}
-                    alt={post.node.frontmatter.title}
-                    title={post.node.frontmatter.title}
-                  />
-                </header>
-                <Content>
-                  <h2>{post.node.frontmatter.title}</h2>
-                  {props.showPostDate ? (
-                    <p>
-                      Escrito el{" "}
-                      <time dateTime={post.node.frontmatter.date}>
-                        {formatDate(post.node.frontmatter.date)}
-                      </time>
-                    </p>
-                  ) : (
-                    ""
-                  )}
-                  <Category>
-                    {post.node.frontmatter.category.map((category, index) => (
-                      <span key={index}>{category}</span>
-                    ))}
-                  </Category>
-                </Content>
-              </article>
-            </Link>
-          </Post>
-        )
-      })}
-    </PostsListContainer>
+  const [searchTerm, setSearchTerm] = React.useState("")
+  const [searchResults, setSearchResults] = React.useState(posts)
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    console.log(searchResults)
+    setSearchTerm(event.target.value)
+  }
+
+  useEffect(() => {
+    const results = posts.filter((post) =>
+      post.node.frontmatter.title
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    )
+    setSearchResults(results)
+  }, [searchTerm])
+
+  return (
+    <>
+      <SearchContainer>
+        <input
+          type="text"
+          placeholder="Buscar entrada..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </SearchContainer>
+
+      <PostsListContainer>
+        {searchResults.slice(0, props.length).map((post) => {
+          let featuredImage =
+            post.node.frontmatter.featuredImage !== null
+              ? post.node.frontmatter.featuredImage.childImageSharp.fixed
+              : useGetGenericFeaturedImage().childImageSharp.fluid
+
+          return (
+            <Post key={post.node.id}>
+              <Link to={`/${post.node.fields.slug}`} key={post.node.id}>
+                <article>
+                  <header>
+                    <Img
+                      fixed={featuredImage}
+                      fadeIn={true}
+                      alt={post.node.frontmatter.title}
+                      title={post.node.frontmatter.title}
+                    />
+                  </header>
+                  <Content>
+                    <h2>{post.node.frontmatter.title}</h2>
+                    {props.showPostDate ? (
+                      <p>
+                        Escrito el{" "}
+                        <time dateTime={post.node.frontmatter.date}>
+                          {formatDate(post.node.frontmatter.date)}
+                        </time>
+                      </p>
+                    ) : (
+                      ""
+                    )}
+                    <Category>
+                      {post.node.frontmatter.category.map((category, index) => (
+                        <span key={index}>{category}</span>
+                      ))}
+                    </Category>
+                  </Content>
+                </article>
+              </Link>
+            </Post>
+          )
+        })}
+      </PostsListContainer>
+    </>
   )
 }
