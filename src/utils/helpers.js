33639b3b1bb527ca5fe75ba9575db894a415817b
Ardillán <ardillan@me.com>
2020-10-28 20:42:23 +0100

    Añade función que formatea slugs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25,3 +25,23 @@ export const formatDateTime = (commitTime) => {
 }
 
 export const shuffleArray = (array) => array.sort(() => Math.random() - 0.5)
+
+export const slugify = (str) => {
+  var map = {
+    "-": " ",
+    "-": "_",
+    a: "á|à|ã|â|À|Á|Ã|Â",
+    e: "é|è|ê|É|È|Ê",
+    i: "í|ì|î|Í|Ì|Î",
+    o: "ó|ò|ô|õ|Ó|Ò|Ô|Õ",
+    u: "ú|ù|û|ü|Ú|Ù|Û|Ü",
+    c: "ç|Ç",
+    n: "ñ|Ñ",
+  }
+
+  for (var pattern in map) {
+    str = str.replace(new RegExp(map[pattern], "g"), pattern)
+  }
+
+  return str
+}
